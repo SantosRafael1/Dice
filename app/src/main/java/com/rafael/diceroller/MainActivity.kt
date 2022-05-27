3557ea10a3cb237c2ee45f342e6af70a70165bde
Rafael Santos <contactrafaelsantos@gmail.com>
2022-05-27 10:33:33 -0300

    Criado o ID do botao e logica basica para gerar um numero aleatorio

--- app/src/main/java/com/rafael/diceroller/MainActivity.kt
+++ app/src/main/java/com/rafael/diceroller/MainActivity.kt
@@ -2,10 +2,24 @@ package com.rafael.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val displayPlayedNumber = findViewById<TextView> (R.id.display_number)
+        val playButton = findViewById<Button> (R.id.play_button)
+        playButton.setOnClickListener {
+            val dice = Dice(6)
+            displayPlayedNumber.text = dice.randomNumber.toString()
+        }
     }
+}
+
+class Dice(private val number: Int) {
+    val randomNumber = (1..number).random()
 }
