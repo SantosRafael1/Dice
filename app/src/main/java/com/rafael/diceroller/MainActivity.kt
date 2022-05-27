794ab67c1cdf6e43dca2f3fe289a3695a327e514
Rafael Santos <contactrafaelsantos@gmail.com>
2022-05-27 11:17:45 -0300

    Lógica para exibir as imagens do dados

--- app/src/main/java/com/rafael/diceroller/MainActivity.kt
+++ app/src/main/java/com/rafael/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rafael.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 
@@ -12,10 +13,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //val displayPlayedNumber = findViewById<TextView> (R.id.display_number)
+        val diceImages = findViewById<ImageView> (R.id.dice_image)
         val playButton = findViewById<Button> (R.id.play_button)
         playButton.setOnClickListener {
             val dice = Dice(6)
             //displayPlayedNumber.text = dice.randomNumber.toString()
+            when(dice.randomNumber) {
+                1 -> diceImages.setImageResource(R.drawable.dice1)
+                2 -> diceImages.setImageResource(R.drawable.dice2)
+                3 -> diceImages.setImageResource(R.drawable.dice3)
+                4 -> diceImages.setImageResource(R.drawable.dice4)
+                5 -> diceImages.setImageResource(R.drawable.dice5)
+                else -> diceImages.setImageResource(R.drawable.dice6)
+            }
         }
     }
 }
